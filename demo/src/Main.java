a33146753c7318d6382740864e41b62e3a690c8e
Abhinna Sundar Behuria <abhinnabehuria@outlook.com>
2025-07-07 05:50:37 +0530

    Initial commit

--- demo/src/Main.java
+++ demo/src/Main.java
@@ -1,39 +1,17 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class Main {
-    // Shared variable, marked as volatile to ensure visibility between threads
-    static volatile boolean flag=false;
 
     public static void main(String[] args) {
-        try{
-            ObjectOutputStream oos=new ObjectOutputStream(new FileOutputStream("file.sc"));
-            oos.writeObject(new Student(1,"ABhi","Password"));
-            ObjectInputStream ois=new ObjectInputStream(new FileInputStream("file.sc"));
-            Student sc=(Student)ois.readObject();
-            System.out.println(sc);
-        }catch (Exception e){
-
+        List<String > list=new ArrayList(Arrays.asList("Abhinna","Bharati","Laxmi","Swopna"));
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()){
+            String next = iterator.next();
+            System.out.println(next);
         }
-    }
-}
-
-class Student implements Serializable{
-    private int id;
-    private String name;
-    private transient String passwoed;
-
-    public Student(int id, String name, String passwoed) {
-        this.id = id;
-        this.name = name;
-        this.passwoed = passwoed;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", passwoed='" + passwoed + '\'' +
-                '}';
     }
 }
